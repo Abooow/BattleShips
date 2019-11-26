7f252ce2430f0cb87ee7b46e881ee004f67e16e7
Daniel Andulahad <daniel00.abdulahad@gmail.com>
2019-11-26 13:56:21 +0100

    Added a preview of the code

--- BattleShips/BattleShips.py
+++ BattleShips/BattleShips.py
@@ -32,3 +32,83 @@ print("Den här texten är också grön")
 print(Color.RESET)
 
 print("Texten är normal igen")
+
+
+
+
+
+
+
+
+
+
+class Ship:
+    def __init__(self, position, lenth, direction):
+        self.position = position
+        self.lenth = lenth
+        self.direction = direction
+
+
+class Board:
+    def __init__(self, size, ships):
+        self.list = [[' '] * size[0] for i in range(size[1])] 
+        self.ships = ships
+        
+
+    def create_num(self):
+        for y in range(len(self.list)):
+            for x in range(len(self.list[y])):
+                if x == 0 or x == len(self.list[y]) - 1 or y == 0 or y == len(self.list) - 1:
+                    self.list[y][x] = char
+
+
+    def create_border(self, char='\u2588'):
+        for y in range(len(self.list)):
+            for x in range(len(self.list[y])):
+                if x == 0 or x == len(self.list[y]) - 1 or y == 0 or y == len(self.list) - 1:
+                    self.list[y][x] = char
+
+
+    def draw(self):
+        for ship in self.ships:
+            for i in range(ship.lenth):
+                x = ship.position[0] + ship.direction[0] * i + 1
+                y = ship.position[1] + ship.direction[1] * i + 1
+                self.list[y][x] = '\u0FD5'
+
+
+        for y in range(len(self.list)):
+            for x in range(len(self.list[y])):
+                print(self.list[y][x], end='')
+            print()
+
+
+def set_color(color):
+    print(color, end='')
+
+def reset_color():
+    print(Color.RESET, end='')
+
+
+def draw_boards_side_by_side(b1, b2, margin=(0, 0, 1)):
+    for i in range(margin[0]):
+        print()
+
+    for y in range(max(len(b1.list), len(b2.list))):
+        print(' ' * margin[1], end='')
+        print(''.join(b1.list[y]), end='')
+        print(' ' * margin[2], end='')
+        print(''.join(b2.list[y]))
+
+
+
+b1 = Board(size=(11, 11), ships=[Ship((0, 0), 4, (1, 0)), Ship((9, 9), 8, (-1, 0)), Ship((5, 2), 2, (0, -1))])
+b1.create_border()
+b2 = Board(size=(11, 11), ships=[Ship((2, 2), 3, (0, -1))])
+b2.create_border()
+
+set_color(Color.LIGHTCYAN)
+b1.draw()
+reset_color()
+print()
+b2.draw()
