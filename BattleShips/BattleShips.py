86f6df1ecd1fff9e8bb4bbb1d7664dd0f00bc0f7
= <oscar.sarnqvist@gmail.com>
2019-12-03 15:01:35 +0100

    Added shooting

--- BattleShips/BattleShips.py
+++ BattleShips/BattleShips.py
@@ -46,14 +46,14 @@ class Ship:
 
 class Board:
     def __init__(self):
-        self.list = [[(' ', Color.WHITE)] * 10 for i in range(10)]
-
+        self.list = [[(' ', Color.WHITE, None)] * 10 for i in range(10)]
+        self.shots_fired = []
 
     def place_ship(self, ship):
         for i in range(ship.length):
             x = ship.position[0] + i * ship.rotation[0]
             y = ship.position[1] + i * ship.rotation[1]
-            self.list[y][x] = ('O', Color.GREEN)
+            self.list[y][x] = ('O', Color.GREEN, ship)
 
 
     def draw(self):
@@ -63,6 +63,25 @@ class Board:
                 Color.print_color('|', Color.LIGHTCYAN, end='')
             print()
 
+    def shoot(self, shot_koord):
+        
+        
+        if shot_koord in self.shots_fired:
+            return False  
+        else:
+            koordinat =  self.list[shot_koord[1]][shot_koord[0]]
+            self.shots_fired.append(shot_koord)
+            if koordinat[2] == None:
+                self.list[shot_koord[1]][shot_koord[0]] = ('*', Color.GREEN,  koordinat[2])
+            else:
+                self.list[shot_koord[1]][shot_koord[0]] = ('O', Color.RED,  koordinat[2])
+            return True
+            
+    
+
+        
+        
+                
 
 player2 = Board()
 player = Board()
@@ -71,5 +90,13 @@ player.place_ship(Ship((5, 2), 3, Ship.DOWN))
 player.place_ship(Ship((0, 0), 4, Ship.RIGHT))
 player.place_ship(Ship((9, 9), 6, Ship.UP))
 
+player.shoot((0,0))
+
+
+
 
 player.draw()
+
+
+
+
