c1c38ca3fd8f5928bc0db3b8116e275b4c3b6cfa
Daniel Andulahad <daniel00.abdulahad@gmail.com>
2019-12-09 14:43:56 +0100

    added screens

---
+++ BattleShips/screens.py
@@ -0,0 +1,51 @@
+class Screen():
+    def __init__(self):
+        pass
+
+
+    def load_content(self):
+        pass
+
+
+    def update(self):
+        pass
+
+
+    def draw(self):
+        pass
+
+
+
+class MenuScreen(Screen):
+    def __init__(self):
+        return super().__init__()
+
+
+    def load_content(self):
+        return super().load_content()
+
+
+    def update(self):
+        return super().update()
+
+
+    def draw(self):
+        return super().draw()
+
+
+
+class PlaceShipsScreen(Screen):
+    def __init__(self):
+        return super().__init__()
+
+
+    def load_content(self):
+        return super().load_content()
+
+
+    def update(self):
+        return super().update()
+
+
+    def draw(self):
+        return super().draw()
