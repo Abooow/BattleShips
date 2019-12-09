f1f840e9ffea331de17f417373b387c798377003
Andreas Landin <andreas.landin.4@gmail.com>
2019-12-09 15:35:34 +0100

    added draw function

--- BattleShips/battleships.py
+++ BattleShips/battleships.py
@@ -194,19 +194,47 @@ class AI:
 
 
 class Cell:
-        #Board håller koll på Cellens position
-        #Board lägger in info i cell om båtar och träffar
-        #innehåller båt?
-        #träffad?
+
+    cellImg = pygame.image.load('Ruta.png')
+    botImg = pygame.image.load('boat_bottom.png')
+    midImg = pygame.image.load('boat_middle.png')
+    topImg = pygame.image.load('boat_top.png')
+
     def __init__(self):
         '''
-        
-        '''
-        self.contains = []
-        self.hit = []
+        param None
+        returns None'''
+
+        self.ship = None
+        self.hit = False
 
     def update(self):
-        pygame.display.update()
-
-    def draw(self):
+        '''
+        '''
         pass
+
+    def draw(self, position, rotation, part):
+        '''
+        param position tuple([int,int]):
+        param rotation tuple([int,int]):
+        param part string(bottom/middle/top):
+        returns None'''
+        x = position[0]
+        y = position[1]
+
+        if rotation == (-1, 0): dir = -90 #LEFT
+        elif rotation == (1, 0): dir = 90 #RIGHT
+        elif rotation == (0, 1): dir = 180 #DOWN
+        else: dir = 0 #UP
+
+        screen.blit(cellImg, (x,y))
+        if self.ship != None and part == 'bottom': 
+            botImg = pygame.transform.rotate(botImg, dir)
+            screen.blit(botImg, (x,y))
+        elif self.ship != None and part == 'middle':
+            midImg = pygame.transform.rotate(midImg, dir)
+            screen.blit(midImg, (x,y))
+        elif self.ship != None and part == 'top':
+            topImg = pygame.transform.rotate(topImg, dir)
+            screen.blit(topImg, (x,y))
+
