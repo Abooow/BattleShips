a4b8ea9a944a9a793ee8d2e7f007c59d7d15468f
Andreas Landin <andreas.landin.4@gmail.com>
2019-12-11 12:37:24 +0100

    window

---
+++ BattleShips/config.py
@@ -0,0 +1,12 @@
+# Configurations for the program 
+
+
+# window size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 480
+window = None
+# the currently running screen
+current_screen = None
+
+# if True, then the program quits
+quit_game = False
