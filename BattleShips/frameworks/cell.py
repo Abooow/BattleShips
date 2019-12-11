a0c3cd64c2df529b5302adfb2fa465973d6a0845
Andreas Landin <andreas.landin.4@gmail.com>
2019-12-11 11:14:10 +0100

    added draw_enemy function

--- BattleShips/frameworks/cell.py
+++ BattleShips/frameworks/cell.py
@@ -1,9 +1,11 @@
 class Cell:
-
-    cellImg = pygame.image.load('Ruta.png')
-    botImg = pygame.image.load('boat_bottom.png')
-    midImg = pygame.image.load('boat_middle.png')
-    topImg = pygame.image.load('boat_top.png')
+    
+    cellImg = pygame.image.load(r'content\sprites\Ruta.png')
+    botImg = pygame.image.load(r'content\sprites\boat_bottom.png')
+    midImg = pygame.image.load(r'content\sprites\boat_middle.png')
+    topImg = pygame.image.load(r'content\sprites\boat_top.png')
+    missImg = pygame.image.load(r'content\sprites\hitmarker.png')
+    hitImg = pygmae.image.load(r'content\sprites\hitmarker_red.png')
 
     def __init__(self):
         '''
@@ -19,11 +21,12 @@ class Cell:
         param rotation tuple([int,int]):
         param part string(bottom/middle/top):
         returns None'''
+
         x = position[0]
         y = position[1]
 
-        # draw cell boarder img
-        screen.blit(cellImg, (x,y))
+        # draw cell boarder img 50x50
+        window.blit(cellImg, (x,y))
 
         # set rotate value for ship img
         if self.ship != None:
@@ -35,11 +38,29 @@ class Cell:
         # draw ship img
         if self.ship != None and part == 'bottom': 
             botImg = pygame.transform.rotate(botImg, dir)
-            screen.blit(botImg, (x,y))
+            window.blit(botImg, (x,y))
         elif self.ship != None and part == 'middle':
             midImg = pygame.transform.rotate(midImg, dir)
-            screen.blit(midImg, (x,y))
+            window.blit(midImg, (x,y))
         elif self.ship != None and part == 'top':
             topImg = pygame.transform.rotate(topImg, dir)
-            screen.blit(topImg, (x,y))
+            window.blit(topImg, (x,y))
 
+    def draw_enemy(self, position):
+        '''
+        param position tuple([int,int]):
+        returns None'''
+
+        x = position[0]
+        y = position[1]
+
+        # draw cell boarder img 50x50
+        window.blit(cellImg, (x,y))
+
+        # draw hitmarker after shot
+        if self.hit == True and self.ship != None: 
+            window.blit(hitImg, (x,y))
+        elif self.hit == True and self.ship == None:
+            window.blit(missImg, (x,y))
+
+    
